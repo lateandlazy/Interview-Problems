8fcd94a3fa13d13a81dfd9111fc564a4042551d2
Rehan Shamim <rehan7050355196@gmail.com>
2025-09-12 11:45:55 +0530

    Time: 8 ms (52.24%), Space: 19.1 MB (25.75%) - LeetHub

---
+++ 3227-vowels-game-in-a-string/3227-vowels-game-in-a-string.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool doesAliceWin(string s) {
+        int count = 0;
+        for(char ch: s){
+            if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
+                count++;
+                if(count > 0)return true;
+            }
+        }
+        return false;
+    }
+};
