a6db61a5ad3b9c5015bf4311c7995c7c8b5b1b61
Rehan Shamim <rehan7050355196@gmail.com>
2025-09-13 12:25:45 +0530

    Time: 4 ms (19.67%), Space: 10.1 MB (22.4%) - LeetHub

--- 3541-find-most-frequent-vowel-and-consonant/3541-find-most-frequent-vowel-and-consonant.cpp
+++ 3541-find-most-frequent-vowel-and-consonant/3541-find-most-frequent-vowel-and-consonant.cpp
@@ -3,23 +3,16 @@ public:
     int maxFreqSum(string s) {
         unordered_map<int,int> co;
         unordered_map<int,int> vo;
+        int ans_c = 0;
+        int ans_v = 0;
         for(char ch: s){
             if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
                 vo[int(ch)] += 1;
+                ans_v = max(ans_v, vo[int(ch)]);
             }
             else{
-            co[int(ch)] += 1;}
-        }
-        int ans_c = 0;
-        int ans_v = 0;
-        for(auto& pair: co){
-            if(pair.second > ans_c){
-                ans_c = pair.second;
-            }
-        }
-        for(auto& pair: vo){
-            if(pair.second > ans_v){
-                ans_v = pair.second;
+            co[int(ch)] += 1;
+            ans_c = max(ans_c, co[int(ch)]);
             }
         }
         return ans_c + ans_v;
