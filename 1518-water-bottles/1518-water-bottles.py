7cc046dfd7aeef8cdf96ecad8c68b0ea7b5b572d
Rehan Shamim <rehan7050355196@gmail.com>
2025-10-02 02:24:03 +0530

    Time: 0 ms (100%), Space: 17.6 MB (88.21%) - LeetHub

---
+++ 1518-water-bottles/1518-water-bottles.py
@@ -0,0 +1,11 @@
+class Solution:
+    def numWaterBottles(self, numBottles: int, numExchange: int) -> int:
+        ans = 0
+        extra = 0
+        while numBottles > 0:
+            ans += numBottles
+            total = numBottles + extra
+            temp = int(total // numExchange)
+            extra = total % numExchange
+            numBottles = temp    
+        return ans 
