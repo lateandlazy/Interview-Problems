913e1da26102171e61355c2d469eefe45dcb2b67
Rehan Shamim <rehan7050355196@gmail.com>
2025-10-02 15:08:39 +0530

    Time: 33 ms (90.56%), Space: 17.9 MB (24.44%) - LeetHub

--- 3100-water-bottles-ii/3100-water-bottles-ii.py
+++ 3100-water-bottles-ii/3100-water-bottles-ii.py
@@ -1,13 +1,9 @@
 class Solution:
     def maxBottlesDrunk(self, numB: int, numE: int) -> int:
-        ans = 0
-        empty = 0
-        while numB > 0:
-            ans += numB
-            empty += numB
-            numB = 0
-            while empty >= numE:
-                empty -= numE
-                numB += 1
-                numE += 1
-        return ans        
+        ans = numB
+        empty = numB
+        while empty >= numE:
+            ans += 1
+            empty -= numE - 1
+            numE += 1      
+        return ans    
