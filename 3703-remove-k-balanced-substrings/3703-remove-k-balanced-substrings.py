5e9c78a15a235ffa53170fc74ca1face0b3d6604
Rehan Shamim <rehan7050355196@gmail.com>
2025-10-05 21:15:35 +0530

    Time: 5077 ms (0%), Space: 19.1 MB (100%) - LeetHub

--- 3703-remove-k-balanced-substrings/3703-remove-k-balanced-substrings.py
+++ 3703-remove-k-balanced-substrings/3703-remove-k-balanced-substrings.py
@@ -1,10 +1,11 @@
 class Solution:
     def removeSubstring(self, s: str, k: int) -> str:
-        temp = ''
+        temp1 = ''
+        temp2 = ''
         for i in range(k):
-            temp += '('
-        for i in range(k):
-            temp += ')'
+            temp1 += '('
+            temp2 += ')'
+        temp = temp1 + temp2    
         while temp in s:
             s = s.replace(temp,"")
         return s        
