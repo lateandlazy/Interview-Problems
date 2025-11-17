93d720fa82273fd8c0172adbd73d20fdf6e33620
Rehan Shamim <rehan7050355196@gmail.com>
2025-11-17 23:54:37 +0530

    Time: 28 ms (5.74%), Space: 24.6 MB (5.74%) - LeetHub

--- 1437-check-if-all-1s-are-at-least-length-k-places-away/1437-check-if-all-1s-are-at-least-length-k-places-away.py
+++ 1437-check-if-all-1s-are-at-least-length-k-places-away/1437-check-if-all-1s-are-at-least-length-k-places-away.py
@@ -5,7 +5,8 @@ class Solution:
         for i in range(n):
             if nums[i] == 1:
                 ar.append(i)
-        if not ar: return False        
+        if not ar: return True
+        if len(ar) == 1 : return True        
         m = len(ar)   
         for i in range(m-1):
             if ar[i+1] - ar[i] - 1 < k: 
