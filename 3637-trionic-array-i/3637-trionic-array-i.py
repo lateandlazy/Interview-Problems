6d464d9303eda9478bc75d9b78832b8547db4ade
Rehan Shamim <rehan7050355196@gmail.com>
2026-02-04 11:36:30 +0530

    Time: 143 ms (8.49%), Space: 19.3 MB (44.41%) - LeetHub

--- 3637-trionic-array-i/3637-trionic-array-i.py
+++ 3637-trionic-array-i/3637-trionic-array-i.py
@@ -2,7 +2,6 @@ class Solution:
     def isTrionic(self, nums: List[int]) -> bool:
         n = len(nums)
         if n < 4: return False
-        fq = defaultdict(int)
         def inc_check(ar):
             if len(ar) == 1: return True
             for i in range(len(ar) - 1):
@@ -15,10 +14,9 @@ class Solution:
                 if ar[i] <= ar[i+1]:
                     return False
             return True                            
-        is_trionic = False
         for i in range(1, n - 2):
             for j in range(i+1, n-1):
-                if inc_check(nums[0:i]) and inc_check(nums[j:n]) and dec_check(nums[i:j]):
+                if inc_check(nums[0:i+1]) and inc_check(nums[j:n]) and dec_check(nums[i:j+1]):
                     return True
         return False            
 
