182d5f507a63c5ecadfce895c21ee3dea25ff485
Rehan Shamim <rehan7050355196@gmail.com>
2026-03-30 16:15:45 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.py
+++ 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.py
@@ -1,22 +1,18 @@
 class Solution:
     def checkStrings(self, s1: str, s2: str) -> bool:
         n = len(s1)
-        ev1 = []
-        ev2 = []
-        od1 = []
-        od2 = []
+        fqe1 = defaultdict(int)
+        fqe2 = defaultdict(int)
+        fqo1 = defaultdict(int)
+        fqo2 = defaultdict(int)
         for i in range(0,n,2):
-            ev1.append(s1[i])
-            ev2.append(s2[i])
+            fqe1[s1[i]] += 1
+            fqe2[s2[i]] += 1
         for i in range(1,n,2):
-            od1.append(s1[i])
-            od2.append(s2[i]) 
-        ev1.sort()
-        ev2.sort() 
-        od1.sort()
-        od2.sort()
-        if ev1 != ev2:
+            fqo1[s1[i]] += 1
+            fqo2[s2[i]] += 1
+        if fqe1 != fqe2:
             return False
-        if od1 != od2:
+        if fqo1 != fqo2:
             return False
         return True              
