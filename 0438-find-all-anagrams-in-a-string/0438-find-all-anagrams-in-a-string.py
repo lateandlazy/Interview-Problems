ea3f53aaa7bc56e92dffe10a8e57f5cfb9c2451b
Rehan Shamim <rehan7050355196@gmail.com>
2026-04-04 10:50:13 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -2,6 +2,7 @@ class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
         n = len(s)
         m = len(p)
+        if n < m:return []
         res = []
         fqs = defaultdict(int)
         fqp = defaultdict(int)
@@ -10,12 +11,12 @@ class Solution:
         for i in range(m):
             fqp[p[i]] += 1
         if fqs == fqp:
-            res.append(0)
-        for i in range(1,n-m+1):
-            fqs[s[i-1]] -= 1
-            fqs[s[i+m-1]] += 1
+            res.append(0)    
+        for i in range(m,n):
+            fqs[s[i]] += 1
+            fqs[s[i-m]] -= 1
             if fqs == fqp:
-                res.append(i)
+                res.append(i-m+1)
         return res
 
         
