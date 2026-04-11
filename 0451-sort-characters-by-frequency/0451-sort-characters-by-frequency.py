b6ad99f082cdfceee6794075b34a75bcd91de8a0
Rehan Shamim <rehan7050355196@gmail.com>
2026-04-11 19:45:05 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
@@ -3,7 +3,11 @@ class Solution:
         fq = defaultdict(int)
         for a in s:
             fq[a] += 1
+        '''
         fq1 = sorted(fq.items(), key =lambda item: item[1], reverse=True)    
+        fq1 = dict(fq1)
+        '''
+        fq1 = sorted(fq.items(), key=lambda x:x[1], reverse=True)
         fq1 = dict(fq1)
         ans = ''
         for a, b in fq1.items():
